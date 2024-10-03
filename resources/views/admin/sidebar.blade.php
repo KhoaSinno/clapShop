92ae030abd733a1495eef6a74637e7a63138d5f1
DatOris <ntdathttt2211003@student.ctuet.edu.vn>
2024-10-03 20:28:40 +0700

    Update sidebar.blade.php

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,14 +1,15 @@
 <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
 <aside class="app-sidebar">
-    <div class="app-sidebar__user"><img class="app-sidebar__user-avatar" src="/e_adminSN/assets/images/avtHotBoy.jpg"
-            width="50px" alt="User Image">
+        <div class="app-sidebar__user">
+                <img class="app-sidebar__user-avatar" src="../e_customerSN/img/vietnamese.jpg"
+                style="width: 100px; height: 100px; object-fit: cover;" alt="User Image">
         <div>
-            <p class="app-sidebar__user-name text-white"><b>Sinoo</b></p>
-            <p class="app-sidebar__user-designation text-white">Wellcome my Boss</p>
+                <p class="app-sidebar__user-name text-white"><b>CLAPSHOP</b></p>
+                <p class="app-sidebar__user-designation text-white">Wellcome my Boss</p>
         </div>
-    </div>
-    <hr>
-    <ul class="app-menu">
+        </div>
+        <hr>
+        <ul class="app-menu">
         <li><a class="app-menu__item haha" href="{{ route('admin.pos') }}"><i class='app-menu__icon bx bx-cart-alt'></i>
                 <span class="app-menu__label">POS Bán Hàng</span></a></li>
 
@@ -24,5 +25,5 @@
         <li><a class="app-menu__item {{ request()->is('admin/category') ? 'active' : '' }} " href="{{ route('admin.category') }}"><i class='app-menu__icon bx bx-id-card'></i>
                 <span class="app-menu__label">Quản lý danh mục</span></a></li>
 
-    </ul>
+        </ul>
 </aside>
